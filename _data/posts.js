399e4bbb6d1b4f891bcd50fb37b89d0c88e6ed67
Tyro Sageframe <tyro@sageframe.net>
2026-03-17 19:24:56 -0400

    Add cover images to writing cards via RSS enclosure

--- _data/posts.js
+++ _data/posts.js
@@ -1,28 +1,24 @@
 const Parser = require('rss-parser');
 
-module.exports = async function () {
-  const parser = new Parser();
+module.exports = async function() {
+  const parser = new Parser({
+    customFields: {
+      item: [['enclosure', 'enclosure', { keepArray: false }]]
+    }
+  });
   const feed = await parser.parseURL('https://sageframe.substack.com/feed');
 
-  return feed.items.slice(0, 6).map(item => {
-    const date = new Date(item.pubDate);
-    const formatted = new Intl.DateTimeFormat('en-US', {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
-    }).format(date);
-
-    const snippet = (item.contentSnippet || item.description || '').trim();
-    const description =
-      snippet.length > 160
-        ? snippet.slice(0, 160).replace(/\s+\S*$/, '') + '\u2026'
-        : snippet;
-
-    return {
+  return feed.items
+    .slice(0, 6)
+    .map(item => ({
       title: item.title,
       url: item.link,
-      date: formatted,
-      description,
-    };
-  });
+      date: new Date(item.pubDate).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      }),
+      description: item.contentSnippet || item.description || '',
+      image: item.enclosure?.url || ''
+    }));
 };
